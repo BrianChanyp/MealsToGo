fc5d8c785c2eefab93ee5c67f90cf3d8e2063294
BrianChanyp <brianchanyp@gmail.com>
2022-09-16 21:44:09 +1200

    Cards added and formating into themes

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components/native";
+
+import { useFonts, Oswald_400Regular } from "@expo-google-fonts/oswald";
+import { useFonts, Lato_400Regular } from "@expo-google-fonts/lato";
+
+import { theme } from "./src/infrastructure/theme";
+import { RestaurantScreen } from "./src/features/restaurants/screens/restarantsScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	const [oswaldLoaded] = useFonts({
+		Oswald_400Regular,
+	});
+	const [latoLoaded] = useFonts({
+		Lato_400Regular,
+	});
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	if (!oswaldLoaded || !latoLoaded) {
+		return null;
+	}
+
+	return (
+		<>
+			<ThemeProvider theme={theme}>
+				<RestaurantScreen />
+			</ThemeProvider>
+			<ExpoStatusBar style="auto" />
+		</>
+	);
+}
